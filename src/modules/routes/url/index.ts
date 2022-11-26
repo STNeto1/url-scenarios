ee3679f16601753741abacd1293995f8b45e068d
STNeto1 <gfreitasneto18@gmail.com>
2022-11-25 22:19:05 -0300

    list urls

--- src/modules/routes/url/index.ts
+++ src/modules/routes/url/index.ts
@@ -9,7 +9,7 @@ import fp from 'fastify-plugin'
 import { z } from 'zod'
 import zodToJsonSchema from 'zod-to-json-schema'
 import { JwtPayload } from '../auth/schemas'
-import { createUrlSchema } from './schemas'
+import { createUrlSchema, paginationSchema, urlListSchema } from './schemas'
 
 export default fp(
   async (server: FastifyInstance, _: FastifyPluginOptions, next: Function) => {
@@ -46,6 +46,55 @@ export default fp(
         })
 
         return reply.status(201).send()
+      }
+    })
+
+    server.route({
+      url: '/v1/url/list',
+      logLevel: 'warn',
+      method: ['GET'],
+      onRequest: [server.authenticate],
+      schema: {
+        params: zodToJsonSchema(paginationSchema, 'paginationSchema'),
+        response: {
+          200: zodToJsonSchema(urlListSchema, 'urlListSchema')
+        }
+      },
+      handler: async (request: FastifyRequest, reply: FastifyReply) => {
+        const payload = request.user as JwtPayload
+        const params = request.params as z.infer<typeof paginationSchema>
+
+        const user = await server.prisma.user.findUnique({
+          where: {
+            id: payload.sub
+          }
+        })
+
+        if (!user) {
+          return reply.status(401).send({
+            message: 'Unauthorized'
+          })
+        }
+
+        const urls = await server.prisma.url.findMany({
+          where: {
+            userId: user.id,
+            deletedAt: null
+          },
+          skip: (params.page - 1) * params.limit,
+          take: params.limit
+        })
+        const count = await server.prisma.url.count({
+          where: {
+            userId: user.id,
+            deletedAt: null
+          }
+        })
+
+        return reply.status(200).send({
+          data: urls,
+          pages: Math.ceil(count / params.limit)
+        })
       }
     })
 
--- src/modules/routes/url/schemas.ts
+++ src/modules/routes/url/schemas.ts
@@ -3,3 +3,20 @@ import { z } from 'zod'
 export const createUrlSchema = z.object({
   url: z.string().url()
 })
+
+export const paginationSchema = z.object({
+  page: z.number().min(1).default(1),
+  limit: z.number().min(1).max(100).default(10)
+})
+
+export const urlListSchema = z.object({
+  pages: z.number(),
+  data: z.array(
+    z.object({
+      id: z.string(),
+      url: z.string(),
+      hash: z.string(),
+      createdAt: z.string()
+    })
+  )
+})
